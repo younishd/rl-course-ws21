5225c5b6fc9c93fcfc32f877027241c6b7c34063
pabair <pabair@users.noreply.github.com>
2021-10-13 15:15:39 +0200

    updated lab2

--- 2_FrozenLake_Prediction.py
+++ 2_FrozenLake_Prediction.py
@@ -28,6 +28,8 @@ def main():
     while successful_episodes > 0:
         rewards = play_episode()
 
+        # Task 1: update Q-values using MC
+
         if sum(rewards) > 0:
             # Task 1: print Q-values using MC
             # Task 2: play 100 episodes using current Q-values and greedy policy
