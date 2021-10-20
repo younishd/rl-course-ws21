a24abf5b095a711725a6cbf5f5e7761075c1d9d7
Younis <younis.bensalah@gmail.com>
2021-10-20 13:47:30 +0200

    Print both mc control and q-learning in same plot

--- 3_FrozenLake_Control.py
+++ 3_FrozenLake_Control.py
@@ -25,11 +25,10 @@ def play_episode(q, eps):
     s_a = []
 
     while not done:
-        # TODO: use q-values to implement epsilon-greedy
         action = (
             lambda s: q[s, :].argmax()
-            if random.random() > eps
-            else random.choice([i for i in range(0, 4) if i != q[s, :].argmax()])
+            if np.random.uniform() > eps
+            else np.random.choice([i for i in range(0, 4) if i != q[s, :].argmax()])
         )(state)
 
         s_a.append((state, action))
@@ -39,28 +38,44 @@ def play_episode(q, eps):
 
 
 def main():
-    no_episodes = 10000
+    alpha = 0.5
+    gamma = 1.0
+    no_episodes = 1000
     plot_data = []
+
+    # mc control
     for eps in [0.01, 0.1, 0.5, 1.0]:
         rewards = []
         for _ in range(0, no_episodes):
             s_a, r_s = play_episode(q_values, eps)
             rewards.append(sum(r_s))
 
-            # TODO: update q-values with MC-prediction
             for i, (s, a) in enumerate(s_a):
                 return_i = sum(r_s[i:])
                 q_counter[s, a] += 1
                 q_values[s, a] += 1 / q_counter[s, a] * (return_i - q_values[s, a])
 
-        plot_data.append((eps, np.cumsum(rewards)))
+        plot_data.append((eps, np.cumsum(rewards), "MC control (eps={})".format(eps)))
+
+    # q-learning
+    for eps in [0.01, 0.1, 0.5, 1.0]:
+        rewards = []
+        for _ in range(0, no_episodes):
+            s_a, r_s = play_episode(q_values, eps)
+            rewards.append(sum(r_s))
+
+            for i, (s, a) in enumerate(s_a):
+                q_counter[s, a] += 1
+                q_values[s, a] += alpha * (r_s[i] + gamma * q_values[s, :].max() - q_values[s, a])
+
+        plot_data.append((eps, np.cumsum(rewards), "Q-learn (eps={})".format(eps)))
 
     # plot the rewards
     plt.figure()
     plt.xlabel("No. of episodes")
-    plt.ylabel("Sum of Rewards")
-    for (eps, data) in plot_data:
-        plt.plot(range(0, no_episodes), data, label="eps={}".format(eps))
+    plt.ylabel("Sum of rewards")
+    for (eps, data, label) in plot_data:
+        plt.plot(range(0, no_episodes), data, label=label)
     plt.legend()
     plt.show()
 
